ce64e3e73ef52dd42df3c5faaf7b2556a71cf54e
marinimau <mauro.marini97@gmail.com>
2021-12-02 11:01:14 +0100

    update configurations

---
+++ my_exp.py
@@ -0,0 +1,12 @@
+#
+#   elliot copyright © 2021 - all rights reserved
+#   Created at: 29/11/21
+#   By: mauromarini
+#   License: MIT
+#   Repository: https://github.com/marinimau/elliot
+#   Credits: @marinimau (https://github.com/marinimau)
+#
+
+from elliot.run import run_experiment
+
+run_experiment("config_files/my_exp.yml")
